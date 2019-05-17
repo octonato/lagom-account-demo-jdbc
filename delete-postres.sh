3a0a7901214455e05a42c619d6a7628d1ff1e25a
Renato Cavalcanti <renato@cavalcanti.be>
2019-05-17 14:55:38 +0200

    remove postgres

--- delete-postres.sh
+++
@@ -1,5 +0,0 @@
-kubectl delete service postgres 
-kubectl delete deployment postgres
-kubectl delete configmap postgres-config
-kubectl delete persistentvolumeclaim postgres-pv-claim
-kubectl delete persistentvolume postgres-pv-volume
